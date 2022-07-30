107453f36c6a0885ab3eec5bb580fd6a0fcb4b7f
Raphael Sampaio <raphael.rss@grao.com.br>
2022-07-30 14:42:02 -0300

    Removing env

--- src/services/index.ts
+++ src/services/index.ts
@@ -16,7 +16,7 @@ interface MoviesResponse {
 }
 
 export async function getMovies () {
-  const API_KEY = '79216e60-59e4-4647-96fb-6e74201a74b5'
+  const API_KEY = ''
 
   const BASE_URL = `https://www.myapifilms.com/imdb/top?token=${API_KEY}&format=json&data=0`
   try {
